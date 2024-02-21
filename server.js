bddca1cb9f438f3a2a5ed532cde1c4667f0ab8bb
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2024-02-21 23:21:56 +0100

    Reset

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require("http");
 const hostname = "0.0.0.0";
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Application listening on port ${port}`);
-});
+const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
